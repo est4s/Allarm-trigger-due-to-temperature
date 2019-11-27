4688cc461f8f4d4f5e5c5d9787a90f64df7c6753
Ensar Yerişenoğlu <53571773+est4s@users.noreply.github.com>
2019-11-27 11:40:25 +0300

    Update codeForAlarm.ino

--- codeForAlarm.ino
+++ codeForAlarm.ino
@@ -1,21 +1,21 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(7, 6, 2, 3, 4, 5);
 LiquidCrystal lcd1( 12,13, 8, 9, 10, 11);
-float critTemp = 40;
+float critTemp = 40; //critical temperature
 
 const int lm350=A0;
 float vin0=0;
-float sicaklik0=0;
-int gelenVeri0=0;
+float temperature0=0;
+int dataIn0=0;
 
 const int lm351=A1;
 float vin1=0;
-float sicaklik1=0;
-int gelenVeri1=0;
+float temperature1=0;
+int dataIn1=0;
 
 float vin=0;
-float sicaklik=0;
-int gelenVeri=0;
+float temperature=0;
+int dataIn=0;
 
 void setup() {
   Serial.begin(9600);
@@ -37,11 +37,11 @@ void setup() {
   delay(1000);
 }
 
-float celciusCevir(float gelenVeri){
-  
-  vin = (gelenVeri / 1023.0) * 5000;
-  sicaklik = vin / 10.0;
-  return sicaklik;
+//converting analog input from lm35 sensor data to celcius value
+float celciusCevir(float dataIn){
+  vin = (dataIn / 1023.0) * 5000;
+  temperature = vin / 10.0;
+  return temperature;
   }
 
 void loop() {
@@ -58,25 +58,24 @@ void loop() {
   lcd1.print("Kabin ici:");
 
   lcd.setCursor(0,1);
-  sicaklik0=celciusCevir(analogRead(lm350));
-  lcd.print(sicaklik0);
+  temperature0=celciusCevir(analogRead(lm350));
+  lcd.print(temperature0);
   lcd.print(" C");
 
   lcd1.setCursor(0,1);
-  sicaklik1 = celciusCevir(analogRead(lm351));
-  lcd1.print(sicaklik1);
+  temperature1 = celciusCevir(analogRead(lm351));
+  lcd1.print(temperature1);
   lcd1.print(" C");
  delay(250);
 
 
- 
-  if(sicaklik0>75.0 || sicaklik1>75.0){ 
+ //conditions for alarm to trigger
+  if(temperature0>critTemp || temperature1>critTemp){ 
     digitalWrite(A2, LOW);
     }
   else{ 
     digitalWrite(A2, HIGH);
     }
 
-    
   delay(250);
 }
